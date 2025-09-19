889ce9129a0695dbc8b66d40a9cc0f06c16381eb
Roger Steve <roxhersbozali@gmail.com>
2025-09-18 22:14:12 -0400

    Added a search bar where a user can look for a specific park

---
+++ TennisApp/src/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {TextInput, StyleSheet, View} from 'react-native';
+
+
+const SearchBar =({ onSearch }) => {
+    return (
+        <View>
+         <TextInput
+            style={styles.input}
+            placeholder="Search for a court..."
+            onChangeText={onSearch}
+      />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    backgroundColor: '#fff',
+  },
+  input: {
+    height: 40,
+    borderColor: '#ddd',
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 10,
+  },
+});
+
+// Ensure the component is the default export
+export default SearchBar;
